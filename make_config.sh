f5262aa5596c00c64149ff98e8d5d369559e7916
mailgannt <72885554+mailgannt@users.noreply.github.com>
2020-11-30 04:42:42 +0400

    Create make_config.sh

---
+++ make_config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# First argument: Client identifier
+
+KEY_DIR=~/vpnclient/keys
+OUTPUT_DIR=~/vpnclient/files
+BASE_CONFIG=~/vpnclient/base.conf
+
+cat ${BASE_CONFIG} \
+    <(echo -e '<ca>') \
+    ${KEY_DIR}/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    ${KEY_DIR}/${1}.crt \
+    <(echo -e '</cert>\n<key>') \
+    ${KEY_DIR}/${1}.key \
+    <(echo -e '</key>\n<tls-crypt>') \
+    ${KEY_DIR}/ta.key \
+    <(echo -e '</tls-crypt>') \
+    > ${OUTPUT_DIR}/${1}.ovpn
